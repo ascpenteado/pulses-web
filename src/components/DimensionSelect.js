5991c113d4cde2c872c068db1b04f19ea59cb313
Arthur Penteado <ascpenteado@gmail.com>
2021-03-21 02:39:58 -0300

    create dimension select component

---
+++ src/components/DimensionSelect.js
@@ -0,0 +1,17 @@
+import { useContext } from "react";
+import { DataContext } from "../contexts/DataContext";
+
+function DimensionSelect() {
+  const { dimensions } = useContext(DataContext);
+  return (
+    <select className="rounded-sm p-2">
+      {dimensions
+        ? dimensions.map((dimension) => {
+            return <option key={dimension.dimensionId}>{dimension.dimensionTitle}</option>;
+          })
+        : "Criar dimensão"}
+    </select>
+  );
+}
+
+export default DimensionSelect;
