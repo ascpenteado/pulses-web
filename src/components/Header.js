f36e6ebee4afc3352f1f7cde51210162ee75cb32
Arthur Penteado <ascpenteado@gmail.com>
2021-03-21 15:08:34 -0300

    remove shadown from header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,8 @@
-function Header() {
+function Header({ children }) {
   return (
-    <header className="bg-white shadow">
-      <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-        <h1 className="text-3xl font-bold text-gray-900">Dashboard</h1>
+    <header className="bg-white">
+      <div className="max-w-7xl mx-auto py-6 mb-4">
+        <h1 className="text-3xl font-bold text-gray-900">{children}</h1>
       </div>
     </header>
   );
