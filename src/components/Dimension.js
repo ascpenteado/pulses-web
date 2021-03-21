8b1307ad1fc40ee4a78c846125b7fc04b528d6e3
Arthur Penteado <ascpenteado@gmail.com>
2021-03-21 01:20:48 -0300

    add dynamic data to dimensions

--- src/components/Dimension.js
+++ src/components/Dimension.js
@@ -1,11 +1,12 @@
 import Btn from "./Btn";
 
-function Dimension() {
+function Dimension({ dimension }) {
   return (
     <div>
       <li className="flex justify-between items-center border-2 mb-2 py-2 px-4 rounded">
-        Estrutura
+        {dimension.dimensionTitle}
         <div>
+          <Btn text="Editar" textColor="text-white" bgColor="bg-blue-500" extraClass="mr-4" />
           <Btn text="Excluir" textColor="text-white" bgColor="bg-red-500" />
         </div>
       </li>
--- src/pages/DimensionsPage.js
+++ src/pages/DimensionsPage.js
@@ -1,8 +1,11 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
 import Btn from "../components/Btn";
 import Dimension from "../components/Dimension";
+import { DataContext } from "../contexts/DataContext";
 
 function DimensionsPage() {
+  const { dimensions } = useContext(DataContext);
   return (
     <div>
       <div className="flex justify-end">
@@ -12,9 +15,11 @@ function DimensionsPage() {
       </div>
       <div>
         <ul className="py-4">
-          <Dimension />
-          <Dimension />
-          <Dimension />
+          {dimensions
+            ? dimensions.map((dimension) => {
+                return <Dimension key={dimension.dimensionId} dimension={dimension} />;
+              })
+            : "Carregando..."}
         </ul>
       </div>
     </div>
